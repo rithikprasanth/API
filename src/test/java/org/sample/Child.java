959742663793c099e90f2028986f470998ab7a4e
rithikprasanth <hrithikprasanth2013@gmail.com>
2023-07-31 14:40:23 +0530

    String added

--- src/test/java/org/sample/Child.java
+++ src/test/java/org/sample/Child.java
@@ -6,11 +6,15 @@ public class Child extends SingleInheritance{
 			System.out.println("method 2");
 			method1();
 		}
+		public void method3() {
+			System.out.println("method 3 ");
+		}
 		
 public static void main(String[] args) {
 			
 			Child c=new Child();
 			c.method2();
+			c.method3();
 		}
 	}
 
